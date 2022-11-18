c7860c24ad2ba460b4dcbd593ce36fcdcdf46576
Valentina-K <kvalentyna@hotmail.com>
2022-11-18 11:15:29 +0200

    Task-02 added second solution

--- js/task-02.js
+++ js/task-02.js
@@ -7,11 +7,23 @@ const ingredients = [
   'Condiments',
 ];
 const ulEl = document.querySelector("#ingredients");
-const liElements = [];
+/* const liElements = [];
 ingredients.forEach((elem) => {
   const liRef = document.createElement("li");
   liRef.textContent = elem;
   liRef.classList.add('item');
   liElements.push(liRef);
 });
+ulEl.append(...liElements); */
+
+const makeLiElements = textContents => {
+  return textContents.map((textContent) => {
+    const liRef = document.createElement("li");
+    liRef.textContent = textContent;
+    liRef.classList.add('item');
+    
+    return liRef;
+  });
+};
+const liElements = makeLiElements(ingredients);
 ulEl.append(...liElements);
