696e54155db9183b5c31980dd14ef4233110218f
Valentina-K <kvalentyna@hotmail.com>
2022-11-18 16:54:36 +0200

    Task-04 is ok

---
+++ js/task-04.js
@@ -0,0 +1,23 @@
+let counterValue = 0;
+const refs = {
+    btnDecr: document.querySelector('[data-action="decrement"]'),
+    btnIncr: document.querySelector('[data-action="increment"]'),
+    counterSpan: document.querySelector('#value')
+}
+console.log(refs.btnDecr);
+refs.btnDecr.addEventListener('click', onButtonDecrClick);
+refs.btnIncr.addEventListener('click', onButtonIncrClick);
+
+function onButtonDecrClick() {
+    counterValue -= 1;
+    refs.counterSpan.textContent = counterValue;
+}
+
+function onButtonIncrClick() {
+    counterValue += 1;
+    refs.counterSpan.textContent = counterValue;
+}
+
+function updateTextContent() {
+    refs.counterSpan.textContent = counterValue;
+}
