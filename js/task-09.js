61542826b3c785c9e51c2a3e3d0c6c5d05a44c1a
Valentina-K <kvalentyna@hotmail.com>
2022-11-19 13:12:37 +0200

    Task-09 is ok

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
+const refs = {
+  body: document.querySelector('body'),
+  span: document.querySelector('.color'),
+  button: document.querySelector('.change-color')
+}
+refs.button.addEventListener('click', onButtonClick);
+function onButtonClick() {
+  refs.span.textContent = getRandomHexColor();
+  refs.body.setAttribute('style', `background-color: ${getRandomHexColor()}`);
+}
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
