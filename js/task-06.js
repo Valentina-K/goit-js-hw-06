78b64b09fcbb0da397863009018cff7b0ea060d4
Valentina-K <kvalentyna@hotmail.com>
2022-11-19 11:57:28 +0200

    Update task-06

--- js/task-06.js
+++ js/task-06.js
@@ -5,23 +5,22 @@ inputRef.addEventListener('blur', onInputBlur);
 function onInputBlur(event) {
     const isInvalid = event.currentTarget.classList.contains('invalid');
     const isValid = event.currentTarget.classList.contains('valid');
-    
-    if(inputRef.value.length === Number(inputRef.dataset.length)){
-    //if (event.currentTarget.value.length === event.target.attributes["data-length"].value) {
+    //if(inputRef.value.length === Number(inputRef.dataset.length)){
+    if (event.currentTarget.value.length === parseInt(event.target.attributes["data-length"].value)) {
         if (!isValid) {
             event.currentTarget.classList.add('valid');
-        } 
+        }
         if (isInvalid) {
             event.currentTarget.classList.remove('invalid');
         }
     }
-    else {       
+    else {
         if (!isInvalid) {
             event.currentTarget.classList.add('invalid');
-        } 
+        }
         if (isValid) {
             event.currentTarget.classList.remove('valid');
         }
     }
-    
+
 }
