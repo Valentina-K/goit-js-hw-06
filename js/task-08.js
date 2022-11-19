4a18f77828ee5a3ec27417897367936efce4ce54
Valentina-K <kvalentyna@hotmail.com>
2022-11-19 13:00:27 +0200

    Task-08 is ok

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,17 @@
-
+const formRef = document.querySelector('.login-form');
+formRef.addEventListener('submit', onFormSubmit);
+function onFormSubmit(event) {
+    event.preventDefault();
+    const inputElements = {
+        email: event.currentTarget.elements['email'].value,
+        password: event.currentTarget.elements['password'].value
+    }
+    if ((inputElements.email === '')
+        || (inputElements.password === '')) {
+        alert('Все поля формы должны быть заполнены!');
+    } else {
+        console.log(inputElements);
+        event.currentTarget.reset();
+    }
+    
+}
